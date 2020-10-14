3b6aa17964d93b2420be015074805243f023b470
u3shadow <u3shadow@gmail.com>
2020-10-14 11:20:46 +0800

    增加提示弹窗

--- app/src/main/java/com/u3coding/remoteadb/MainActivity.kt
+++ app/src/main/java/com/u3coding/remoteadb/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.u3coding.remoteadb
 
+import android.content.Context
+import android.content.DialogInterface
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.text.format.Formatter
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.io.DataOutputStream
+import java.io.File
 import java.io.IOException
-import kotlin.collections.ArrayList
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val commands = arrayListOf(
-            "/system/bin/sh",
-            "setprop service.adb.tcp.port 5555",
-            "stop adbd",
-            "start adbd"
-        )
-        try {
-            RunAsRoot(commands)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+            val wm = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+            val ip: String = Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
+            AlertDialog.Builder(this).setMessage("该程序仅工作在拥有root权限设备上，如果没有root，请连接usb并使用下列命令进行远程调试\nadb tcpip 5555\nadb connect $ip:5555").setPositiveButton(
+                "确认"
+            ) { _, _ ->
+                val commands = arrayListOf(
+                    "/system/bin/sh",
+                    "setprop service.adb.tcp.port 5555",
+                    "stop adbd",
+                    "start adbd"
+                )
+                try {
+                    RunAsRoot(commands)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.show()
+
     }
 
     private fun RunAsRoot(cmds: ArrayList<String>) {
@@ -43,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         val ip: String = Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
         AlertDialog.Builder(this).setMessage("设置完成，请在同一局域网下使用以下命令连接设备\nadb connect ip $ip:5555").setPositiveButton(
             "确认"
-        ) { p0, p1 -> finish() }.show()
+        ) { _, _ -> finish() }.show()
     }
 
 }
